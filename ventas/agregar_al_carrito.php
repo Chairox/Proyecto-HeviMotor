4b73bef294a8800753022c4eb779ddcc62a48d93
Álvaro Goez <goezalvaro237@gmail.com>
2024-03-15 11:19:34 -0500

    Delete ventas directory

--- ventas/agregar_al_carrito.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "funciones.php";
-if (!isset($_POST["id_repuesto"])) {
-    exit("No hay id_repuesto");
-}
-agregarProductoAlCarrito($_POST["id_repuesto"]);
-header("Location: tienda.php");
--- ventas/form.php
+++
@@ -1,7 +0,0 @@
-
-<form action="agregar_al_carrito.php" method="post">
-    <input type="hidden" name="id_repuesto" value="<?php echo $repuesto->id ?>">
-    <button class="button is-primary">
-        <i class="fa fa-cart-plus"></i>&nbsp;Agregar al carrito
-    </button>
-</form>
